6c4cc66860989c1791f521332dd97b74173e1ad7
ronald-bog <105521717+ronald-bog@users.noreply.github.com>
2023-06-13 23:15:51 -0500

    Update exam.java

--- exam.java
+++ exam.java
@@ -12,67 +12,37 @@ public class Circulo implements Forma{
 
 public class Rectangulo implements Forma{
 	@Override
-
 	public void dibujar(){
-
 	    System.out.println('Dibujando la forma');
-
 	}
-
 }
-
-
 
 public class Cuadrado implements Forma{
-
 	@Override
-
 	public void dibujar(){
-
 	    System.out.println('Dibujando la forma');
-
 	}
-
 }
 
-
-
 public class ShapeMaker{
-
 	public void drawCircle(Circulo circulo){
-
 		circulo.dibujar();
-
 	}
-
+	
 	public void drawRectangle(Rectangulo rectangulo){
-
 		rectangulo.dibujar();
-
 	}
 
 	public void drawSquare(Cuadrado cuadrado ){
-
 		 cuadrado.dibujar();
-
 	}
-
 }
 
-
-
 public class FacadeDemo{
-
 	public void principal{
-
 		ShapeMaker shapeMaker = new ShapeMaker ();
-
 		shapeMaker.drawCircle();
-
 		shapeMaker.drawRectangle();
-
 		shapeMaker.drawSquare();
-
 	}
-
 }
